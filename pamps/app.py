2e8d9898b893eb6568b1829c775252147b57d5df
Alan S <alnjsantos@hotmail.com>
2023-01-05 12:11:54 +0000

    config container completed

---
+++ pamps/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='Pamps',
+    version='0.1.0',
+    description='Pamps is a posting app'
+)
+
+@app.get('/')
+async def index():
+    return {"Hello": "World"}
